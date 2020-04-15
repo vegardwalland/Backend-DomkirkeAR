26a87935052731a6f99551123d0f0f1186ee076a
Vegard Walland <vegard.walland@gmail.com>
2020-04-15 23:10:20 +0200

    Add next/link to header links

--- components/Header.js
+++ components/Header.js
@@ -2,6 +2,7 @@ import '../styles/style.css';
 import cookie from 'js-cookie';
 import Router from 'next/router';
 import { checkLogin } from '../libs/helperFunctions'
+import Link from 'next/link'
 
 
 //Check if a user is logged in
@@ -9,21 +10,27 @@ let showLogoutButton = checkLogin();
 
 const Header = () => (
   <div className="border-b border-blue-500 w-full">
-        <a className="nav-item ml-4" href="/">Home</a>
-        <a className="nav-item" href="/add">Add</a>
-        <a className="nav-item" href="/items">Browse</a>
-        <div className="float-right mr-2">
-          {/*Only show logout button if user is logged in*/}
-          {showLogoutButton && (
-            <button className="nav-item"
-            onClick={() => {
-              cookie.remove('token');
-              Router.push('/');
-            }}>
-            Logg ut
-          </button>
-          )}
-        </div>
+    <Link href="/">
+      <a className="nav-item ml-4">Home</a>
+    </Link>
+    <Link href="/add">
+      <a className="nav-item">Add</a>
+    </Link>
+    <Link href="/items">
+      <a className="nav-item">Browse</a>
+    </Link>
+    <div className="float-right mr-2">
+      {/*Only show logout button if user is logged in*/}
+      {showLogoutButton && (
+        <button className="nav-item"
+        onClick={() => {
+          cookie.remove('token');
+          Router.push('/');
+        }}>
+        Logg ut
+      </button>
+      )}
+    </div>
   </div>
 );
 

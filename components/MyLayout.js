7d1dd822d041297b0df2b2cf4d13c2533db57600
Vegard Walland <vegard.walland@gmail.com>
2020-05-14 13:04:48 +0200

    Add cookie banner, GDPR, and som styling

--- components/MyLayout.js
+++ components/MyLayout.js
@@ -4,7 +4,7 @@ export default function Layout(props) {
     return (
         <div className="bg-gray-200 w-full h-screen">
             <Header />
-            <div className="ml-6 mr-6 text-blue-700">
+            <div className=" mr-6 text-blue-700">
                 {props.children}
             </div>
         </div>
---
+++ pages/privacy-notice.js
@@ -0,0 +1,59 @@
+import Layout from "../components/MyLayout";
+import Head from 'next/head';
+
+
+
+function PrivacyPolicy() {
+
+    return (
+        <Layout>
+            <div className="block text-center text-xs font-bold">
+                <Head>
+                    <title>Gamle Stavanger AR</title>
+                    <meta classname="viewport" content="initial-scale=1.0, width=device-width" />
+                </Head>
+                <main>
+                    <h1 className="text-3xl my-8">Privacy Policy</h1>
+                    <div>
+                        <h2 className="my-5 text-gray-700 text-sm">How do we collect your data?</h2>
+                        <p>
+                        You directly provide the data we collect. We collect data and process data when you
+                        sign up, log in and create or update items.
+                        </p>
+                        <h2 className="my-5 text-gray-700 text-sm">How will we use your data?</h2>
+                        <p>
+                            Your data will never be sold. <br/>
+                            Your email is only used to authenticate log in and reset password. <br/>
+                            Item data is only used to create AR objects in Gamle Stavanger AR Android application
+                        
+                        </p>
+                        <h2 className="my-5 text-gray-700 text-sm">How do we use cookies?</h2>
+                        <p>
+                            To keep you logged in and to verify user access <br/>
+                        </p>
+                        <h2 className="my-5 text-gray-700 text-sm">What are your data protection rights?</h2>
+                        <ul>
+                            <h3 className="mb-1 text-gray-700 ">Every user is entitled to the following:</h3><br/>
+
+                            <li>The right to access – You have the right to request copies of your personal data.
+                            </li>
+                            <li>The right to rectification – You have the right to request a correction of any information you believe is inaccurate. You also have the right to request to complete the information you believe is incomplete. <br/>
+                            </li>
+                            <li>The right to erasure – You have the right to request an erase of your personal data, under certain conditions.
+                            </li>
+                            <li>The right to restrict processing – You have the right to request that we restrict the processing of your personal data, under certain conditions.
+                            </li>
+                            <li>The right to object to processing – You have the right to object to our processing of your personal data, under certain conditions.
+                            </li>
+                            <li>The right to data portability – You have the right to request that we transfer the data that we have collected to another organization, or directly to you, under certain conditions.
+                            </li>                                                 
+                        </ul>
+                        <h3 className="my-2 text-gray-700 ">Contact us at: { process.env.ADMIN_EMAIL  }</h3>
+                    </div>
+                </main>
+            </div>
+        </Layout>
+    )
+}
+
+export default PrivacyPolicy;
